2ae9028a8439b227d9934299efcba8dfc8baa631
papayajaia <jpaklossy@gmail.com>
2018-06-18 23:03:53 +0000

    Done.

---
+++ aboutme.rb
@@ -0,0 +1,8 @@
+def about_me
+puts "My name is Jaia Papaya Aginsky."
+puts "I love to play volleyball"
+puts "I am going into 8th grade, although I could be a freshman...."
+puts "I like to read, draw, and craft stuff."
+end
+
+about_me
--- macarena.rb
+++ macarena.rb
@@ -15,16 +15,17 @@ puts "Step 2: Left arm out in front of you, palm facing down."
 puts "Step 3: Place your right hand on your left shoulder."
 
 # Step 4: FIXME
-puts "Step 4: Turn your left palm up."
+puts "Step 4: Place your left hand on your right shoulder, turn your left palm up."
 
 # Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
+puts "Step 5: Place your right hand on the right side of your head."
 
 # Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
+puts "Step 6: Place your left hand on the left side of your head."
 
 # Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+puts "Step 7: Place your right hand on your left hip."
 
 # Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Step 8: Place your left hand on your right hip."
+ 
